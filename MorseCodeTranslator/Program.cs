fae5280f3ea0df2ccb0d18fe0daee4862d160816
Eric <wunderkammer123@gmail.com>
2018-04-10 13:35:03 -0400

    Coded up Main to run Translator

--- MorseCodeTranslator/Program.cs
+++ MorseCodeTranslator/Program.cs
@@ -10,6 +10,21 @@ namespace MorseCodeTranslator
     {
         static void Main(string[] args)
         {
+
+            while (true)
+            {
+                Console.Write(": ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                string output = Translator.ToMorse(input);
+
+                Console.WriteLine(output);
+            }
         }
     }
 }
